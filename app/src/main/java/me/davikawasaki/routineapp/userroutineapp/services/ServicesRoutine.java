b3b3f18f02fb5f83523ac3a11d44bcd738bff360
Davi Kawasaki <davishinjik@gmail.com>
2017-11-21 13:36:04 -0200

    RemoveRoutines from Database and list filter between two lists

--- app/src/main/java/me/davikawasaki/routineapp/userroutineapp/services/ServicesRoutine.java
+++ app/src/main/java/me/davikawasaki/routineapp/userroutineapp/services/ServicesRoutine.java
@@ -2,6 +2,10 @@ package me.davikawasaki.routineapp.userroutineapp.services;
 
 import android.content.Context;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import me.davikawasaki.routineapp.userroutineapp.config.DatabaseHelper;
 import me.davikawasaki.routineapp.userroutineapp.model.Routine;
 
@@ -30,6 +34,30 @@ public class ServicesRoutine {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static void removeRoutines(List<Routine> routineList, Context context) {
+
+        try {
+            DatabaseHelper connection = DatabaseHelper.getInstance(context);
+            connection.getRoutineDAO().delete(routineList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Routine> iterateAndRemoveFromList(List<Routine> fullList,
+                                                         List<Routine> removedList) {
+
+        List<Routine> updatedRoutine = new ArrayList<>(fullList);
+
+        for ( Routine rr : removedList ) {
+            for ( Routine r : fullList ) {
+                if(rr.getId() == r.getId()) updatedRoutine.remove(rr);
+            }
+        }
+
+        return updatedRoutine;
     }
 
 }
