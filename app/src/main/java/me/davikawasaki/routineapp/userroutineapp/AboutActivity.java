9d4b5563a9a352db287e4a2c8b708424c97b0b50
Davi Kawasaki <davishinjik@gmail.com>
2017-11-21 11:05:52 -0200

    Application About Activity

---
+++ app/src/main/java/me/davikawasaki/routineapp/userroutineapp/AboutActivity.java
@@ -0,0 +1,32 @@
+package me.davikawasaki.routineapp.userroutineapp;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setIcon(R.drawable.ic_back_white);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
